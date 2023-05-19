3fa288bc874c42f7924f4133afac0a087fb10e5c
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-05-19 18:48:59 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+    
+    array = arguments[0];
 
+    for (let i = 1; i < arguments.length; i++)
+    {
+        array = array.filter(element => element !== arguments[i]);
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
