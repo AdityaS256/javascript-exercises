76622057b52a68993f3851304a4b372afcd8c811
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-05-19 18:20:56 +0530

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    
+    let solution = "";
+    
+    for (let i = 0; i < num; i++)
+    {
+        solution += string;
+    }
 
+    return solution;
 };
 
 // Do not edit below this line
