66e31e4755019bbb1ecbecd3ed281c84f214358d
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-05-19 18:36:01 +0530

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let solution = "";
 
+    let length = string.length - 1;
+
+    for (let i = length; i >= 0; i--)
+    {
+        solution += string[i];
+    }
+
+    return solution;
 };
 
 // Do not edit below this line
