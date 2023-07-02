3a9a528375aaced596701d21f26d38d5f04397d0
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-07-02 17:22:33 +0530

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regex = /[^a-z]/g;
+    str = str.toLowerCase();
+    str = str.replace(regex, "");
+    let rev = str.split("").reverse().join("");
+    return str === rev;
 };
 
 // Do not edit below this line
