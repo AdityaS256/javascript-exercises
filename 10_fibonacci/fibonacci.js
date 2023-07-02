4d9f03da39b7d8535269f0f10dc31893fffd4f73
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-07-02 18:18:43 +0530

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index < 0) {
+        return "OOPS";
+    }
 
+    let fib = [0, 1];
+    let arr = [];
+    
+    for (let i = 1; i <= index; i++) {
+        arr = fib.slice(-2);
+        // console.log(arr);
+
+        fib.concat(arr);
+        // console.log(fib);
+
+        fib.push(arr[0] + arr[1]);
+        // console.log(fib);
+    }
+    return fib[index];
 };
 
 // Do not edit below this line
