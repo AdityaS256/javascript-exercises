8a46964a48fb8e1c34cd4e3b4d802de08ae85b1a
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-07-02 18:36:55 +0530

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = {
+        name: "",
+        yearsLived: 0,
+    };
 
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = (new Date()).getFullYear();
+        }
+        let yearsLived = person.yearOfDeath - person.yearOfBirth;
+        if (yearsLived > oldest.yearsLived) {
+            oldest.name = person.name;
+            oldest.yearsLived = yearsLived;
+        }
+    })
+
+    return oldest;
 };
 
 // Do not edit below this line
